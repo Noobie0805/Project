25b98f5aa70a5fff745576339e48c2a8969b54f4
Noobie0805 <154042631+Noobie0805@users.noreply.github.com>
2025-08-17 10:31:04 +0530

    Setup routes

--- src/app.js
+++ src/app.js
@@ -14,4 +14,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-export default { app };
+//routes import 
+import userRouter from './routes/user.routes.js';
+
+//routes declaration
+app.use("/api/v1/users", userRouter);   // when user sends a req ending with /users, it will be handled by userRouter
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -28,9 +28,13 @@ const app = express();
 })();
 */
 import dotenv from 'dotenv';
-dotenv.config();
-
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+dotenv.config({
+    path: "./.env"
+});
+
+
 
 connectDB()
     .then(
