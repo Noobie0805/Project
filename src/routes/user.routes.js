5c201cd63ecba9d19daa6612ec615f7c181cd434
Noobie0805 <154042631+Noobie0805@users.noreply.github.com>
2025-09-05 10:20:57 +0530

    Added new routes for user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { registerUser, userlogin, userlogout, refreshAccessToken } from "../controllers/user.controller.js";
+import {
+    registerUser,
+    userlogin,
+    userlogout,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -15,6 +27,14 @@ router.route("/register").post(
 router.route("/login").post(userlogin);
 router.route("/logout").post(verifyJWT, userlogout);  // verifyJWT before logout
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);  //.get is used as we are fetching data only and not changing anything
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);  //.patch is used as we are updating few fields of the user and not the entire user
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
+
 export default router;
 
 /* 
